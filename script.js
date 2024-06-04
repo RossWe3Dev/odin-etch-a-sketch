3b5735cdf8c1b2525f341a4183e0f6ae70ca624a
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-06-04 16:51:01 +0200

    Set up 'hover' effect
    Added event handlers for 'mouseover' event, to each empty div, that
    adds .black-square class to the white squares.

--- script.js
+++ script.js
@@ -7,5 +7,8 @@ for (i = 0; i < gridSize; i++) {
     gridDiv.classList.add("square");
     gridDiv.classList.add("grid");
     container.appendChild(gridDiv);
-    console.log(i);
+
+    gridDiv.addEventListener('mouseover', () => {
+        gridDiv.classList.add("black-square");
+    });
 }
