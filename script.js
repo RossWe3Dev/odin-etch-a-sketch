540ef30c7aa6c2645fde21afc8ef0cea6c979a07
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-06-05 11:10:46 +0200

    Edit the UI

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-const container = document.querySelector(".container");
+const grid = document.querySelector(".grid");
 
-let gridSize = 256;
+let gridSize = 16;
 
-for (i = 0; i < gridSize; i++) {
-    const gridDiv = document.createElement("div");
-    gridDiv.classList.add("square");
-    gridDiv.classList.add("grid");
-    container.appendChild(gridDiv);
+function createGridSquare() {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.classList.add("grid-borders");
+    // Update CSS variable in .grid > .square
+    grid.style.setProperty('--grid-size', gridSize);
 
-    gridDiv.addEventListener('mouseover', () => {
-        gridDiv.classList.add("black-square");
+    square.addEventListener('mouseover', () => {
+        square.classList.add("black-square");
     });
+
+    return square;
+}
+
+for (i = 0; i < (gridSize * gridSize); i++) {
+    const square = createGridSquare();
+    grid.appendChild(square);
 }
